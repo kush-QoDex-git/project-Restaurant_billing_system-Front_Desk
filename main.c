69a22939e7102e21c4cb6a350319d5b03606172b
kush-QoDex-git <kushagra.awasthi.official@gmail.com>
2025-11-18 16:24:28 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,13 @@
+/* Restaurant Billing System (with Menu Categories & Discounts)
+ • Menu divided into categories (Starters, Main Course, Dessert).
+ • Orders stored in arrays/structures.
+ • Apply discount coupons or GST.
+ • Generate final bill with date and time.
+ • Store daily sales in a file for reporting.
+ */
+#include "./dependencies/UI.h"
+int main(){
+    welcome();
+
+    return 0;
+}
